1ff43dc96170fcbf4fe5044a6a57ed22db4a026a
johkade <johkade.dev@gmail.com>
2023-05-07 15:24:15 +0200

    chore: initial commit

---
+++ android/src/main/java/com/verzettled/VerzettledPackage.kt
@@ -0,0 +1,17 @@
+package com.verzettled
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class VerzettledPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(VerzettledModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  VerzettledExample
+//
+
+import Foundation
