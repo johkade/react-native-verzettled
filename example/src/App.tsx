163af3798884bb9b193706743c36d37c561c78f3
johkade <johkade.dev@gmail.com>
2023-05-08 07:56:00 +0200

    chore: add values from config

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,26 +2,27 @@ import * as React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
 import { initZettle, say, showSettingsView } from 'react-native-verzettled';
+import Config from './config.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text
-        style={{ marginBottom: 50 }}
+        style={styles.button}
         onPress={async () => {
-          const result = await say('99999999-9999-9999-9999-999999999999');
+          const result = await say(Config.clientId);
           console.log(result);
         }}
       >
         say
       </Text>
       <Text
-        style={{ marginBottom: 50 }}
+        style={styles.button}
         onPress={async () => {
           try {
             const result = await initZettle(
-              '2328be19-2f9a-481f-a26e-34a879f9b8d1', //5
-              'zettledemo://zettlecallback'
+              Config.clientId,
+              Config.callbackURL
             );
             console.log(result);
           } catch (error) {
@@ -33,7 +34,7 @@ export default function App() {
         init
       </Text>
       <Text
-        style={{ marginBottom: 50 }}
+        style={styles.button}
         onPress={async () => {
           const result = await showSettingsView('');
           console.log(result);
@@ -52,9 +53,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'white',
   },
-  box: {
-    width: 60,
-    height: 60,
+  button: {
     marginVertical: 20,
   },
 });
